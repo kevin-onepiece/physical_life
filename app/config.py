31b4e7b3be1e8424e839c99b945885bb5af27ab4
fuqiang <kevinfu2048@foxmail.com>
2025-07-19 14:55:36 +0800

    fix(system): 修改配置

--- app/config.py
+++ app/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:12345678@localhost:3306/jobs'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://db:EHhasz3ehxiZcr4i@localhost:3306/db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
